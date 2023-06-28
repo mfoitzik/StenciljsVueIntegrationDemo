da9ba5b29eaa316d5861943760a66e36a612391a
Mike Foitzik <mike@mifo.com>
2023-06-28 12:12:29 -0700

    config changes

--- packages/vue-library/dist-transpiled/components.d.ts
+++ packages/vue-library/dist-transpiled/components.d.ts
@@ -1,2 +1,2 @@
-import type { JSX } from 'stencil-library';
+import type { JSX } from '@mifotest/stencil-library/loader';
 export declare const MyComponent: (props: JSX.MyComponent & import("./vue-component-lib/utils").InputProps<string | number | boolean> & {}) => any;
--- packages/vue-library/rollup.config.js
+++ packages/vue-library/rollup.config.js
@@ -4,14 +4,14 @@ export default {
   input: 'dist-transpiled/index.js',
   output: [
     {
-      dir: 'dist/',
+      dir: 'dist2/',
       entryFileNames: '[name].esm.js',
       chunkFileNames: '[name]-[hash].esm.js',
       format: 'es',
       sourcemap: true,
     },
     {
-      dir: 'dist/',
+      dir: 'dist3/',
       format: 'commonjs',
       generatedCode: {constBindings: true},
       sourcemap: true,
