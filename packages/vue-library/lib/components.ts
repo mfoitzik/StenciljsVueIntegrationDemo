e049d11c169b80447abb537d592a5043a1f06821
Mike Foitzik <mike@mifo.com>
2023-06-28 15:14:03 -0700

    tested my-app

--- packages/vue-library/lib/components.ts
+++ packages/vue-library/lib/components.ts
@@ -3,7 +3,7 @@
 /* auto-generated vue proxies */
 import { defineContainer } from './vue-component-lib/utils';
 
-import type { JSX } from '@mifotest/stencil-library/dist/esm';
+import type { JSX } from '@mifotest/stencil-library/loader';
 
 
 
